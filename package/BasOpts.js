90ee44e6e35324bfeeb87c7546079a78fbe77a96
Ungigdu <Ungigdu@protonmail.com>
2020-04-29 18:39:42 +0800

    temp commit

---
+++ package/BasOpts.js
@@ -0,0 +1,31 @@
+//contract abi
+const BasToken_abi = require("./BasToken.json").abi;
+const BasExpiredOwnership_abi = require("./BasExpiredOwnership.json").abi;
+const BasTradableOwnership_abi = require("./BasTradableOwnership.json").abi;
+const BasRootDomain_abi = require("./BasRootDomain.json").abi;
+const BasSubDomain_abi = require("./BasSubDomain.json").abi;
+const BasDomainConf_abi = require("./BasDomainConf.json").abi;
+const BasAccountant_abi = require("./BasAccountant.json").abi;
+const BasMiner_abi = require("./BasMiner.json").abi;
+const BasOANN_abi = require("./BasOANN.json").abi;
+const BasMarket_abi = require("./BasMarket.json").abi;
+const BasMail_abi = require("./BasMail.json").abi;
+const BasMailManager_abi = require("./BasMailManager.json").abi;
+
+//contract address
+const BasToken_address = "";
+const BasExpiredOwnership_address = "";
+const BasTradableOwnership_address = "";
+const BasRootDomain_address = "";
+const BasSubDomain_address = "";
+const BasDomainConf_address = "";
+const BasAccountant_address = "";
+const BasMiner_address = "";
+const BasOANN_address = "";
+const BasMarket_address = "";
+const BasMail_address = "";
+const BasMailManager_address = "";
+
+//recover contracts
+
+
